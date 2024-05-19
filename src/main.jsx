82c3fd2dbf5a7a3f6fec78d8bbb89c56a4ee38c0
sbennett <susannah-bennett@hotmail.com>
2024-05-20 00:24:08 +0100

    Basic portfolio site structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./assets/css/index.css";
+import routes from "./routes.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Root from "../components/Root/Root";
+import Home from "../components/Home/Home";
+import About from "../components/About/About";
+
+const routes = [
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+];
+
+export default routes;
